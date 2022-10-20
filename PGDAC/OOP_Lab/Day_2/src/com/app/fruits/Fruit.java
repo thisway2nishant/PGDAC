494132a9333f4592566783a3e5154a44524ba953
Nishant <thisway2nishant@gmail.com>
2022-10-20 11:58:40 +0530

    Parent Fruit class created.

---
+++ PGDAC/OOP_Lab/Day_2/src/com/app/fruits/Fruit.java
@@ -0,0 +1,41 @@
+package com.app.fruits;
+
+public class Fruit {
+	// static
+
+	// Data Members
+	private String name;
+	private String color;
+	private double weight;
+	private boolean fresh;
+
+	// Constructor
+	public Fruit(String name, String color, double weight) {
+		this.name = name;
+		this.color = color;
+		this.weight = weight;
+		this.fresh = true;
+	}
+
+	// methods
+	public String toString() {
+		return "Name: " + this.name + " Color: " + this.color + " Weight: " + this.weight;
+	}
+
+	public String taste() {
+		return "No Specific Taste.";
+	}
+
+	// Getters and Setters
+	public boolean getFresh() {
+		return fresh;
+	}
+	
+	public void setFresh(boolean n) {
+		this.fresh = n;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
